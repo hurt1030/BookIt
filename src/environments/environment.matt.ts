51db20b59faf173640ca259644b92bc7ffbdd22d
Matthew Hurt <mhurt2018@gmail.com>
2023-11-06 14:38:37 -0600

    add matt configuration

--- src/environments/environment.matt.ts
+++ src/environments/environment.matt.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  firebaseUrl: 'https://springfield-2023-default-rtdb.firebaseio.com/books.json',
+  firebaseUrl: 'https://springfield-2023-d02aa-default-rtdb.firebaseio.com/books.json',
   production: false,
   signUpUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC8RWrfS5G_USZegu0g6PWwtQA7M4sx6O4',
   loginUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC8RWrfS5G_USZegu0g6PWwtQA7M4sx6O4'
